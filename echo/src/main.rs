00af3ee98bd950b0fcfa286fead00781343582d2
YabseraBogale <yabserapython@gmail.com>
2025-07-14 18:55:51 +0300

    echo inital idea

--- echo/src/main.rs
+++ echo/src/main.rs
@@ -1,6 +1,6 @@
-use std::env::{self, args};
+use std::env::args;
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = args().collect();
     for i in args {
         print!("{}", i);
     }
