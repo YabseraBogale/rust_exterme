eb268e2fbd7b48c8538d11c8225bc783f96dcbf8
YabseraBogale <yabserapython@gmail.com>
2025-07-14 20:01:22 +0300

    echo inital idea

--- head/src/main.rs
+++ head/src/main.rs
@@ -1,3 +1,11 @@
+use std::env::args;
 fn main() {
-    println!("Hello, world!");
+    // format for head
+    // head -n line_number file_name
+
+    let args: Vec<String> = args().collect();
+
+    if args[0] == "-n" {
+        println!("ok");
+    }
 }
