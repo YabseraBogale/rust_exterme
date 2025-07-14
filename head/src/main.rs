4fc8f1bcfc9878efa23d135316089f12c7fbfe94
YabseraBogale <yabserapython@gmail.com>
2025-07-14 20:47:23 +0300

    echo inital idea

--- head/src/main.rs
+++ head/src/main.rs
@@ -1,11 +1,17 @@
 use std::env::args;
+use std::fs;
 fn main() {
     // format for head
-    // head -n line_number file_name
+    // head file_name
 
     let args: Vec<String> = args().collect();
-
-    if args[1] == "-n" {
-        println!("ok");
+    let content = fs::read_to_string(args[1].as_str()).expect("File does not exist");
+    let mut counter = 0;
+    for i in content.split("\n") {
+        counter += 1;
+        println!("{}", i);
+        if counter == 5 {
+            break;
+        }
     }
 }
