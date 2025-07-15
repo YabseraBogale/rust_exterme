99b3800cf40e0a11cc7bf9ec5a28e5c3d83f8a5b
YabseraBogale <yabserapython@gmail.com>
2025-07-15 19:08:37 +0300

    echo inital idea

--- pwd/src/main.rs
+++ pwd/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let pwd = env::current_dir();
+    match pwd {
+        Ok(path) => {
+            if let Some(p) = path.to_str() {
+                println!("{}", p);
+            }
+        }
+        Err(error) => {
+            println!("{}", error);
+        }
+    }
 }
